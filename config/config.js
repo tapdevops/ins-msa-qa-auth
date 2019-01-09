06ebbd4883889d5465225c631ffb97101dd5b71e
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-09 15:34:29 +0700

    Ferdinand

--- config/config.js
+++ config/config.js
@@ -47,19 +47,19 @@ module.exports = {
 			ldap: 'http://tap-ldapdev.tap-agri.com/login'
 			*/
 
-			/*
+			
 			hectare_statement: 'http://app.tap-agri.com/mobileinspection/ins-msa-hectarestatement',
 			inspection: 'http://app.tap-agri.com/mobileinspection/ins-msa-inspection',
 			finding: 'http://app.tap-agri.com/mobileinspection/ins-msa-finding',
 			images: 'http://app.tap-agri.com/mobileinspection/ins-msa-images',
-			ldap: 'http://tap-ldapdev.tap-agri.com/login',
-			*/
-			
+			ldap: 'http://tap-ldapdev.tap-agri.com/login'
+			/*
 			hectare_statement: 'http://149.129.245.230:3009',
 			inspection: 'http://149.129.245.230:3010/inspection',
 			finding: 'http://149.129.245.230:3011',
 			images: 'http://149.129.245.230:3012',
 			ldap: 'http://tap-ldapdev.tap-agri.com/login'
+			*/
 			
 		}
 	},
