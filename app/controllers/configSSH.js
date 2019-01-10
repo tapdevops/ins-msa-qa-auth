d49ca246a5bb4317888b7addb9d206cae9a95516
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-10 10:58:32 +0700

    Ferdinand

--- app/controllers/configSSH.js
+++ app/controllers/configSSH.js
@@ -1,10 +1,23 @@
+const scp2 = require( 'scp2' );
 /**
  * syncMobile
  * Untuk menyediakan data mobile
  * --------------------------------------------------------------------------
  */
 exports.init = ( req, res ) => {
+	//const server = scp2.defaults({
+	//	port: 22,
+	//	host: '149.129.245.230',
+	//	username: 'root',
+	//	password: 'T4pagri123'
+	//});
+	//server.scp('root:T4pagri123@149.129.245.230:/Database-Config.txt', 'file.txt', , function(err) {
+	//	if ( err ) {
+	//		console.log( err )
+	//	}
+	//})
+
 	res.json( {
-		message: "Success! Config database berhasil dirubah."
+		message: "Success! Config database berhasil dirubah.s"
 	} );
 }
--- config/database.js
+++ config/database.js
@@ -4,5 +4,7 @@ module.exports = {
 	| MongoDB URL
 	|--------------------------------------------------------------------------
 	*/
-	url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_auth?authSource=admin'
+	//url: 'mongodb://user-db:tap123456me@clustertest-shard-00-01-iwwfu.gcp.mongodb.net:27017/s_auth?authSource=admin'
+	url: 'mongodb://udb_inspeksi:tapinspeksiprod@dbapp.tap-agri.com:27017/s_auth?authSource=admin',
+	ssl: false
 }
