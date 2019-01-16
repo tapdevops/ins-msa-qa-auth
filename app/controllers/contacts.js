d816e1f973d2a17bb0a5d1661bdb918ba9272f29
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-16 11:21:54 +0700

    Ferdinand

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -29,7 +29,10 @@
  * Untuk mengambil data contact berdasarkan location code
  * --------------------------------------------------------------------------
  */
-	exports.find = async ( req, res ) => {
+
+
+
+	exports.find = ( req, res ) => {
 		// Auth Data
 		var auth = req.auth;
 
@@ -107,76 +110,114 @@
 					} );
 				break;
 			}
+
+			/**
+			 * --------------------------------------------------------------------------
+			 * Check Job Code
+			 *
+			 * Note :
+			 * - ASISTEN_LAPANGAN hanya bisa assign ke sesama ASISTEN_LAPANGAN.
+			 * - ASISTEN_KEPALA hanya bisa assign ke ASISTEN_LAPANGAN sesuai USER_ROLE,
+			 *	 tidak bisa ke sesama ASISTEN_LAPANGAN.
+			 * - Selain ASISTEN_LAPANGAN dan ASISTEN_KEPALA, data tidak dimunculkan.
+			 * --------------------------------------------------------------------------
+			 */
+			console.log( auth );
+			if ( auth.USER_ROLE == 'ASISTEN_KEPALA' || auth.USER_ROLE == 'ASISTEN_LAPANGAN' ) {
 			
-			viewUserAuthModel.find( { 
-				$or: query_search
-				//$or: [
-				//	{ LOCATION_CODE_REGION: /^04/ },
-				//	{ LOCATION_CODE_COMP: /^41/ },
-				//	{ LOCATION_CODE_BA: /^4121/ },
-				//	{ LOCATION_CODE_AFD: /^4121/ }
-				//]
-			}  )
-			.select( {
-				USER_AUTH_CODE: 1,
-				EMPLOYEE_NIK: 1,
-				USER_ROLE: 1,
-				LOCATION_CODE: 1,
-				REF_ROLE: 1,
-				PJS_JOB: 1,
-				PJS_FULLNAME: 1,
-				HRIS_JOB: 1,
-				HRIS_FULLNAME: 1,
-				//LOCATION_CODE_NATIONAL: 1,
-				//LOCATION_CODE_REGION: 1,
-				//LOCATION_CODE_COMP: 1,
-				//LOCATION_CODE_BA: 1,
-				//LOCATION_CODE_AFD: 1
-			} )
-			.then( data => {
-				if( !data ) {
-					return res.send( {
-						status: false,
-						message: config.error_message.find_404,
-						data: {}
+				viewUserAuthModel.find( { 
+
+					$and : [
+						{ 
+							$or : query_search
+						},
+						{
+							USER_ROLE : "ASISTEN_LAPANGAN"
+						}
+					]
+
+					// Backup
+					//$or: query_search
+					//$or: [
+					//	{ LOCATION_CODE_REGION: /^04/ },
+					//	{ LOCATION_CODE_COMP: /^41/ },
+					//	{ LOCATION_CODE_BA: /^4121/ },
+					//	{ LOCATION_CODE_AFD: /^4121/ }
+					//]
+				}  )
+				.select( {
+					USER_AUTH_CODE: 1,
+					EMPLOYEE_NIK: 1,
+					USER_ROLE: 1,
+					LOCATION_CODE: 1,
+					REF_ROLE: 1,
+					PJS_JOB: 1,
+					PJS_FULLNAME: 1,
+					HRIS_JOB: 1,
+					HRIS_FULLNAME: 1,
+					//LOCATION_CODE_NATIONAL: 1,
+					//LOCATION_CODE_REGION: 1,
+					//LOCATION_CODE_COMP: 1,
+					//LOCATION_CODE_BA: 1,
+					//LOCATION_CODE_AFD: 1
+				} )
+				.then( data => {
+					if( !data ) {
+						return res.send( {
+							status: false,
+							message: config.error_message.find_404,
+							data: {}
+						} );
+					}
+
+					var results = [];
+					data.forEach( function( result ) {
+						var result = Object.keys(result).map(function(k) {
+							return [+k, result[k]];
+						});
+						result = result[3][1];
+						results.push( {
+							USER_AUTH_CODE: result.USER_AUTH_CODE,
+							EMPLOYEE_NIK: result.EMPLOYEE_NIK,
+							USER_ROLE: result.USER_ROLE,
+							LOCATION_CODE: result.LOCATION_CODE,
+							REF_ROLE: result.REF_ROLE,
+							JOB: ( !result.PJS_JOB ) ? result.HRIS_JOB : result.PJS_JOB,
+							FULLNAME: ( !result.PJS_FULLNAME ) ? result.HRIS_FULLNAME : result.PJS_FULLNAME
+						} );
 					} );
-				}
 
-				var results = [];
-				data.forEach( function( result ) {
-					var result = Object.keys(result).map(function(k) {
-						return [+k, result[k]];
-					});
-					result = result[3][1];
-
-					//if ( result.USE )
-
-					results.push( {
-						USER_AUTH_CODE: result.USER_AUTH_CODE,
-						EMPLOYEE_NIK: result.EMPLOYEE_NIK,
-						USER_ROLE: result.USER_ROLE,
-						LOCATION_CODE: result.LOCATION_CODE,
-						REF_ROLE: result.REF_ROLE,
-						JOB: ( !result.PJS_JOB ) ? result.HRIS_JOB : result.PJS_JOB,
-						FULLNAME: ( !result.PJS_FULLNAME ) ? result.HRIS_FULLNAME : result.PJS_FULLNAME
+					res.send( {
+						status: true,
+						message: config.error_message.find_200,
+						data: results
+					} );
+				} ).catch( err => {
+					res.send( {
+						status: false,
+						message: config.error_message.find_500 + ' - 2',
+						data: []
 					} );
 				} );
-
+			}
+			else {
 				res.send( {
 					status: true,
 					message: config.error_message.find_200,
-					data: results
+					data: []
 				} );
-			} ).catch( err => {
-				res.send( {
-					status: false,
-					message: config.error_message.find_500 + ' - 2',
-					data: {}
-				} );
-			} );
+			}
 
 		}
 		else {
+			res.send( {
+				status: true,
+				message: config.error_message.find_200,
+				data: []
+			} );
+
+			/*
+			Backup
 			viewUserAuthModel.find({})
 			.select( {
 				USER_AUTH_CODE: 1,
@@ -213,6 +254,7 @@
 					data: {}
 				} );
 			} );
+			*/
 		}
 	};
 
