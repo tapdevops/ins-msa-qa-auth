245ca1016b25af12cf8f7c97e92452188377a132
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-17 13:31:01 +0700

    F

--- app/controllers/userSearch.js
+++ app/controllers/userSearch.js
@@ -56,19 +56,20 @@
 				},
 				$and: [
 					{
-						"EMPLOYEE_NIK": { 
-							$regex: q 
-						}
+						$or: [
+							{
+								EMPLOYEE_NIK: { 
+									$regex: new RegExp( '^' + q )
+								}
+							},
+							{
+								EMPLOYEE_FULLNAME: {
+									$regex: new RegExp( '^' + q )
+								}
+							}
+						]
 					}
 				]
-			//	$or: [ 
-			//		{
-			//			"EMPLOYEE_NIK": { 
-			//				$regex: q 
-			//			}
-			//		}
-			//	]
-				
 			} )
 			.sort( {
 				'EMPLOYEE_NAME': 1
@@ -85,11 +86,20 @@
 				EMPLOYEE_NAME: {
 					$exists: true
 				},
-				$or: [ 
+				$and: [
 					{
-						"EMPLOYEE_NIK": { 
-							$regex: q 
-						}
+						$or: [
+							{
+								EMPLOYEE_NIK: { 
+									$regex: new RegExp( '^' + q )
+								}
+							},
+							{
+								EMPLOYEE_NAME: {
+									$regex: new RegExp( '^' + q )
+								}
+							}
+						]
 					}
 				]
 				
@@ -124,193 +134,24 @@
 		}
 
 		// Loop Data SAP
-		data_sap.forEach( function( result ) {
-			var result = Object.keys(result).map(function(k) {
-				return [+k, result[k]];
-			});
-			result = result[3][1];
-			results.push( {
-				NIK: result.NIK,
-				NAMA_LENGKAP: result.EMPLOYEE_NAME,
-				JOB_CODE: result.JOB_CODE
+		if ( data_sap.length > 0 ) {
+			data_sap.forEach( function( result ) {
+				var result = Object.keys(result).map(function(k) {
+					return [+k, result[k]];
+				});
+				result = result[3][1];
+				results.push( {
+					NIK: result.NIK,
+					NAMA_LENGKAP: result.EMPLOYEE_NAME,
+					JOB_CODE: result.JOB_CODE
+				} );
 			} );
-		} );
+		}
 
 		res.send( {
 			status: true,
 			message: config.error_message.find_200,
 			data: results
 		} );
-
-	};
-
-	exports.find2 = ( req, res ) => {
-
-		if( !req.query.q || !req.query.type ) {
-			return res.send({
-				status: false,
-				message: 'Invalid parameter',
-				data: {}
-			});
-		}
-
-		var url_query = req.query;
-		var url_query_length = Object.keys( url_query ).length;
-		var q = url_query.q;
-		var type = url_query.type;
-
-		if ( type == 'SAP' ) {
-			employeeSAPModel.find( { 
-				EMPLOYEE_NAME: {
-					$exists: true
-				},
-				$or: [ 
-					{
-						"EMPLOYEE_NIK": { 
-							$regex: q 
-						}
-					}
-				]
-				
-			} )
-			.sort( {
-				'EMPLOYEE_NAME': 1
-			} )
-			.select( {
-				_id: 0,
-				NIK: 1,
-				EMPLOYEE_NIK: 1,
-				EMPLOYEE_NAME: 1,
-				JOB_CODE: 1,
-			} )
-			.limit( 20 )
-			.then( data => {
-				if( !data ) {
-					return res.send( {
-						status: false,
-						message: 'Data not found 2',
-						data: {}
-					} );
-				}
-				
-				var results = [];
-				data.forEach( function( result ) {
-					var result = Object.keys(result).map(function(k) {
-						return [+k, result[k]];
-					});
-					result = result[3][1];
-					results.push( {
-						NIK: result.EMPLOYEE_NIK,
-						NIK_DISPLAY: result.NIK,
-						NAMA_LENGKAP: result.EMPLOYEE_NAME,
-						JOB_CODE: result.JOB_CODE
-					} );
-				} );
-				res.send( {
-					status: true,
-					message: config.error_message.find_200,
-					data: results
-				} );
-
-			} ).catch( err => {
-				if( err.kind === 'ObjectId' ) {
-					return res.send( {
-						status: false,
-						message: 'Data not found 1',
-						data: {}
-					} );
-				}
-				return res.send( {
-					status: false,
-					message: 'Error retrieving data',
-					data: {}
-				} );
-			} );
-		}
-		else if ( type == 'HRIS' ) {
-			employeeHRISModel.find( { 
-				EMPLOYEE_USERNAME: {
-					$exists: true
-				},
-				EMPLOYEE_NIK: {
-					$exists: true
-				},
-				$and: [
-					{
-						"EMPLOYEE_NIK": { 
-							$regex: q 
-						}
-					}
-				]
-				/*$or: [ 
-					{
-						"EMPLOYEE_NIK": { 
-							$regex: q 
-						}
-					}
-				]*/
-				
-			} )
-			.sort( {
-				'EMPLOYEE_NAME': 1
-			} )
-			.select( {
-				_id: 0,
-				EMPLOYEE_NIK: 1,
-				EMPLOYEE_FULLNAME: 1,
-				EMPLOYEE_POSITION: 1
-			} )
-			.limit( 20 )
-			.then( data => {
-				if( !data ) {
-					return res.send( {
-						status: false,
-						message: 'Data not found 2',
-						data: {}
-					} );
-				}
-				
-				var results = [];
-				data.forEach( function( result ) {
-					var result = Object.keys( result ).map( function( k ) {
-						return [+k, result[k]];
-					});
-					result = result[3][1];
-					results.push( {
-						NIK: result.EMPLOYEE_NIK,
-						NIK_DISPLAY: result.EMPLOYEE_NIK,
-						NAMA_LENGKAP: result.EMPLOYEE_FULLNAME,
-						JOB_CODE: result.EMPLOYEE_POSITION
-					} );
-				} );
-				
-				res.send( {
-					status: true,
-					message: config.error_message.find_200,
-					data: results
-				} );
-
-			} ).catch( err => {
-				if( err.kind === 'ObjectId' ) {
-					return res.send( {
-						status: false,
-						message: 'Data not found 1',
-						data: {}
-					} );
-				}
-				return res.send( {
-					status: false,
-					message: 'Error retrieving data',
-					data: {}
-				} );
-			} );
-		}
-		else {
-			res.send( {
-				status: false,
-				message: config.error_message.find_200 + 'Error',
-				data: []
-			} );
-		}
 
 	};
