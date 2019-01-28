a084caed0432e591687ad0c7bcfafa395e0177ad
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-01-28 10:28:46 +0700

    Ferdinand

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -29,7 +29,67 @@
  * Untuk mengambil data contact berdasarkan location code
  * --------------------------------------------------------------------------
  */
-	exports.find = ( req, res ) => {
+
+ 	exports.find = ( req, res ) => {
+		// Auth Data
+		var auth = req.auth;
+
+		var location_code_group = auth.LOCATION_CODE.split( ',' );
+		var ref_role = auth.REFFERENCE_ROLE;
+		
+		viewUserAuthModel.find({})
+		.select( {
+			USER_AUTH_CODE: 1,
+			EMPLOYEE_NIK: 1,
+			USER_ROLE: 1,
+			LOCATION_CODE: 1,
+			REF_ROLE: 1,
+			PJS_JOB: 1,
+			PJS_FULLNAME: 1,
+			HRIS_JOB: 1,
+			HRIS_FULLNAME: 1
+		} )
+		.then( data => {
+			if( !data ) {
+				return res.send( {
+					status: false,
+					message: config.error_message.find_404,
+					data: {}
+				} );
+			}
+
+			var results = [];
+			data.forEach( function( result ) {
+				var result = Object.keys(result).map(function(k) {
+					return [+k, result[k]];
+				});
+				result = result[3][1];
+				results.push( {
+					USER_AUTH_CODE: result.USER_AUTH_CODE,
+					EMPLOYEE_NIK: result.EMPLOYEE_NIK,
+					USER_ROLE: result.USER_ROLE,
+					LOCATION_CODE: result.LOCATION_CODE,
+					REF_ROLE: result.REF_ROLE,
+					JOB: ( !result.PJS_JOB ) ? result.HRIS_JOB : result.PJS_JOB,
+					FULLNAME: ( !result.PJS_FULLNAME ) ? result.HRIS_FULLNAME : result.PJS_FULLNAME
+				} );
+			} );
+
+			res.send( {
+				status: true,
+				message: config.error_message.find_200,
+				data: results
+			} );
+		} ).catch( err => {
+			res.send( {
+				status: false,
+				message: config.error_message.find_500 + ' - 2',
+				data: []
+			} );
+		} );
+	};
+
+	exports.find2 = ( req, res ) => {
 		// Auth Data
 		var auth = req.auth;
 
