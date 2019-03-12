d23749a89b8a10467968337affab7285e4850b43
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-12 11:55:01 +0700

    ferdinand

--- config/config.js
+++ config/config.js
@@ -44,7 +44,7 @@ module.exports = {
 			hectare_statement: 'http://app.tap-agri.com/mobileinspection/ins-msa-hectarestatement',
 			inspection: 'http://app.tap-agri.com/mobileinspection/ins-msa-inspection',
 			finding: 'http://app.tap-agri.com/mobileinspection/ins-msa-finding',
-			images: 'http://149.129.250.199:3012',
+			images: 'http://149.129.245.230:3012',
 			ldap: 'http://tap-ldapdev.tap-agri.com/login',
 
 			
