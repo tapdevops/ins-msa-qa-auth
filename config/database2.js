dadbb970b95a427791d228e9606d9a0c798a91d9
Ferd Shinoda <mac@Macs-MacBook-Pro.local>
2019-03-14 14:35:48 +0700

    Ferdinand

---
+++ config/database2.js
@@ -0,0 +1,15 @@
+/*
+|--------------------------------------------------------------------------
+| Module Exports
+|--------------------------------------------------------------------------
+*/
+	module.exports = {
+		production: {
+			url: 'mongodb://dbapp:dbapp123@dbapp.tap-agri.com:27017/s_auth?authSource=admin',
+			ssl: false
+		},
+		development: {
+			url: 'mongodb://dbapp:dbapp123@149.129.249.18:27017/s_auth?authSource=admin',
+			ssl: false
+		}
+	}
