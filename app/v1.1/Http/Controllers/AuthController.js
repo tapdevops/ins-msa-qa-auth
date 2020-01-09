0b7c66fb304a6801397b61537b79df10716c923e
ihsanhusaeri <ihusaeri@gmail.com>
2020-01-09 20:17:01 +0700

    delete remark

--- app/v1.1/Http/Controllers/AuthController.js
+++ app/v1.1/Http/Controllers/AuthController.js
@@ -38,130 +38,103 @@
 	  * --------------------------------------------------------------------
 	*/
 		exports.contacts = async ( req, res ) => {
-			// Models.ViewUserAuth.find(
+			Models.ViewUserAuth.find(
 
-			// )
-			// .select( {
-			// 	USER_AUTH_CODE: 1,
-			// 	EMPLOYEE_NIK: 1,
-			// 	USER_ROLE: 1,
-			// 	LOCATION_CODE: 1,
-			// 	REF_ROLE: 1,
-			// 	PJS_JOB: 1,
-			// 	PJS_FULLNAME: 1,
-			// 	HRIS_JOB: 1,
-			// 	HRIS_FULLNAME: 1,
-			// 	INSERT_TIME: 1,
-			// 	UPDATE_TIME: 1,
-			// 	DELETE_TIME: 1
-			// } )
-			// .then( data => {
-			// 	if( !data ) {
-			// 		return res.send( {
-			// 			status: false,
-			// 			message: config.error_message.find_404,
-			// 			data: {}
-			// 		} );
-			// 	}
+			)
+			.select( {
+				USER_AUTH_CODE: 1,
+				EMPLOYEE_NIK: 1,
+				USER_ROLE: 1,
+				LOCATION_CODE: 1,
+				REF_ROLE: 1,
+				PJS_JOB: 1,
+				PJS_FULLNAME: 1,
+				HRIS_JOB: 1,
+				HRIS_FULLNAME: 1,
+				INSERT_TIME: 1,
+				UPDATE_TIME: 1,
+				DELETE_TIME: 1
+			} )
+			.then( data => {
+				if( !data ) {
+					return res.send( {
+						status: false,
+						message: config.error_message.find_404,
+						data: {}
+					} );
+				}
 				
-			// 	var results = [];
-			// 	data.forEach( function( result ) {
-			// 		var result = Object.keys(result).map(function(k) {
-			// 			return [+k, result[k]];
-			// 		});
-			// 		result = result[3][1];
-			// 		var JOB = '';
-			// 		var FULLNAME = '';
-			// 		var location_code = result.LOCATION_CODE + '';
-			// 		var location_code_regional = '';
+				var results = [];
+				data.forEach( function( result ) {
+					var result = Object.keys(result).map(function(k) {
+						return [+k, result[k]];
+					});
+					result = result[3][1];
+					var JOB = '';
+					var FULLNAME = '';
+					var location_code = result.LOCATION_CODE + '';
+					var location_code_regional = '';
 					
-			// 		if ( result.PJS_JOB ) { JOB = result.PJS_JOB; }
-			// 		else if( result.HRIS_JOB ) { JOB = String( result.HRIS_JOB ); }
+					if ( result.PJS_JOB ) { JOB = result.PJS_JOB; }
+					else if( result.HRIS_JOB ) { JOB = String( result.HRIS_JOB ); }
 			
-			// 		if ( result.PJS_FULLNAME ) { FULLNAME = result.PJS_FULLNAME; }
-			// 		else if( result.HRIS_FULLNAME ) { FULLNAME = result.HRIS_FULLNAME; }
+					if ( result.PJS_FULLNAME ) { FULLNAME = result.PJS_FULLNAME; }
+					else if( result.HRIS_FULLNAME ) { FULLNAME = result.HRIS_FULLNAME; }
 
-			// 		var i = 0;
-			// 		location_code.split( ',' ).forEach( function( lc ) {
-			// 			var first_char = lc.substr( 0, 1 );
-			// 			if ( lc != 'ALL' ) {
-			// 				if ( i == 0 ) {
-			// 					if ( first_char != '0' ) {
-			// 						location_code_regional += '0' + lc.substr( 0, 1 );
-			// 					}
-			// 					else {
-			// 						location_code_regional += lc;
-			// 					}
-			// 				}
-			// 				else {
-			// 					if ( first_char != '0' ) {
-			// 						location_code_regional += ',0' + lc.substr( 0, 1 );
-			// 					}
-			// 					else {
-			// 						location_code_regional += ',' + lc;
-			// 					}
-			// 				}
-			// 			}
-			// 			else {
-			// 				location_code_regional = 'ALL';
-			// 			}
-			// 			i++;
-			// 		} );
+					var i = 0;
+					location_code.split( ',' ).forEach( function( lc ) {
+						var first_char = lc.substr( 0, 1 );
+						if ( lc != 'ALL' ) {
+							if ( i == 0 ) {
+								if ( first_char != '0' ) {
+									location_code_regional += '0' + lc.substr( 0, 1 );
+								}
+								else {
+									location_code_regional += lc;
+								}
+							}
+							else {
+								if ( first_char != '0' ) {
+									location_code_regional += ',0' + lc.substr( 0, 1 );
+								}
+								else {
+									location_code_regional += ',' + lc;
+								}
+							}
+						}
+						else {
+							location_code_regional = 'ALL';
+						}
+						i++;
+					} );
 
-			// 		if ( FULLNAME != '' ) {
-			// 			results.push( {
-			// 				USER_AUTH_CODE: result.USER_AUTH_CODE,
-			// 				EMPLOYEE_NIK: result.EMPLOYEE_NIK,
-			// 				USER_ROLE: result.USER_ROLE,
-			// 				LOCATION_CODE: String( result.LOCATION_CODE ),
-			// 				REF_ROLE: result.REF_ROLE,
-			// 				JOB: JOB,
-			// 				FULLNAME: FULLNAME,
-			// 				REGION_CODE: location_code_regional
-			// 			} );
-			// 		}
-			// 	} );
-				
-			// 	res.send( {
-			// 		status: true,
-			// 		message: config.app.error_message.find_200,
-			// 		data: results
-			// 	} );
-			// } ).catch( err => {
-			// 	res.send( {
-			// 		status: false,
-			// 		message: config.app.error_message.find_500,
-			// 		data: []
-			// 	} );
-			// } );
-			let data = await Models.LoginLog.aggregate( [
-				{
-					$group: {
-						"_id": "$USER_AUTH_CODE",
-						"ACCESS_TOKEN": {"$last":"$ACCESS_TOKEN"},
-						"EMPLOYEE_NIK": {"$last": '$EMPLOYEE_NIK'},
-						"USERNAME": {"$last": '$USERNAME'},
-						"IMEI": {"$last": '$IMEI'},
-						"DATE_LOGIN": { "$last": '$DATE_LOGIN'}
+					if ( FULLNAME != '' ) {
+						results.push( {
+							USER_AUTH_CODE: result.USER_AUTH_CODE,
+							EMPLOYEE_NIK: result.EMPLOYEE_NIK,
+							USER_ROLE: result.USER_ROLE,
+							LOCATION_CODE: String( result.LOCATION_CODE ),
+							REF_ROLE: result.REF_ROLE,
+							JOB: JOB,
+							FULLNAME: FULLNAME,
+							REGION_CODE: location_code_regional
+						} );
 					}
-				},
-				{
-					$project: {
-					 	_id: 0,
-						"USER_AUTH_CODE": "$_id",
-						"EMPLOYEE_NIK": "$EMPLOYEE_NIK",
-						"USERNAME": "$USERNAME",
-						"IMEI": "$IMEI",
-						"DATE_LOGIN": "$DATE_LOGIN",
-						"ACCESS_TOKEN": "$ACCESS_TOKEN"
-					   }
-					}  
-		 ] )
-		 res.send( {
-			 data
-		 } );
+				} );
+				
+				res.send( {
+					status: true,
+					message: config.app.error_message.find_200,
+					data: results
+				} );
+			} ).catch( err => {
+				res.send( {
+					status: false,
+					message: config.app.error_message.find_500,
+					data: []
+				} );
+			} );
 		};
-
 
  	/** 
  	  * Login
