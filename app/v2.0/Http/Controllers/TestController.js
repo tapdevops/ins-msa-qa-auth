359eb9beff64a9116dde7815f83bbf47b303f30e
ihsanhusaeri <ihusaeri@gmail.com>
2020-03-12 09:31:42 +0700

    find HRIS di api test

--- app/v2.0/Http/Controllers/TestController.js
+++ app/v2.0/Http/Controllers/TestController.js
@@ -6,8 +6,12 @@
  | Untuk menghandle models, libraries, helper, node modules, dan lain-lain
  |
  */
+    const Models = {
+        EmployeeHRIS: require( _directory_base + '/app/v2.0/Http/Models/EmployeeHRISModel.js' )
+    }
  	exports.test = async (req, res) => {
-         console.log("Testing 1..2..3.. percobaan")
+         let data = await Models.EmployeeHRIS.findOne({EMPLOYEE_NIK: "00000077"})
+         console.log(data)
          return res.send({
              status: true,
              message: "Testing success!",
