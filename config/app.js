c646c85aeaecdb53788f6cd0e80d32445656189c
ihsanhusaeri <ihusaeri@gmail.com>
2020-04-28 09:09:05 +0700

    edit url msa_internal

--- config/app.js
+++ config/app.js
@@ -60,7 +60,7 @@
 					microservice_images: 'http://image.tap-agri.com:4012',
 					microservice_reports: 'http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-reports',
 					microservice_point: 'http://app.tap-agri.com/mobileinspectiondev/ins-msa-dev-point',
-					msa_internal_tap: 'http://msadev.tap-agri.com/ins/mob-api'
+					msa_internal_tap: 'http://msadev.tap-agri.com/ins/mob_ins'
 					
 				},
 				qa: {
@@ -73,7 +73,7 @@
 					microservice_images: 'http://image.tap-agri.com:5012',
 					microservice_reports: 'http://app.tap-agri.com/mobileinspectionqa/ins-msa-qa-reports',
 					microservice_point: 'http://app.tap-agri.com/mobileinspectionqa/ins-msa-qa-point',
-					msa_internal_tap: 'http://msaqa.tap-agri.com/ins/mob-api'
+					msa_internal_tap: 'http://msaqa.tap-agri.com/ins/mob_ins'
 				},
 				prod: {
 					ldap: 'http://tap-ldap.tap-agri.com/login',
