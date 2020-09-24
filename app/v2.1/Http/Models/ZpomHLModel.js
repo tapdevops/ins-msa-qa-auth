88e7f491f3ae7b11b92a7bdc8b4220d63d5abaa9
ihsanhusaeri <ihusaeri@gmail.com>
2020-09-24 16:31:50 +0700

    updated

--- app/v2.1/Http/Models/ZpomHLModel.js
+++ app/v2.1/Http/Models/ZpomHLModel.js
@@ -30,8 +30,8 @@ const ZpomHL = Mongoose.Schema({
 			return 0;
 		}
 	},
-	UPDATE_BY: String,
-	UPDATE_DATE: {
+	UPDATED_BY: String,
+	UPDATED_DATE: {
 		type: Number,
 		get: v => Math.floor(v),
 		set: v => Math.floor(v),
