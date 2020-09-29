83ef61e2301312081173669bfc44daa71351fa02
ihsanhusaeri <ihusaeri@gmail.com>
2020-09-29 15:58:44 +0700

    prod

--- app/v2.0/Http/Controllers/SyncMobileController.js
+++ app/v2.0/Http/Controllers/SyncMobileController.js
@@ -1780,7 +1780,7 @@
 						} );
 					}
 					if ( config.app.env == 'prod' ) {
-						var path_global = 'http://apis.tap-agri.com' + '/' + config.app.path.qa + '/';
+						var path_global = 'http://apis.tap-agri.com' + '/' + config.app.path.prod + '/';
 						// var path_global = req.protocol + '://' + req.get( 'host' ) + '/' + config.app.path.prod + '/';
 					}
 					else if ( config.app.env == 'qa' ) {
